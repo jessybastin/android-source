74fec6f3051dbff6a46e887fae55b5d6e613d376
Jessy Bastin <sjessy@gmail.com>
2015-01-24 15:32:24 -0800

    Checkpint 14 completed

--- foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
+++ foundation/14-best-practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -1,8 +1,5 @@
 package com.bloc.bestpractices;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class WorstPractices extends Object {
 
@@ -12,29 +9,31 @@ public class WorstPractices extends Object {
 	/************************************************/
 
 	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
-	MAGIC_NUMBER--;
-	}
-	}
+		int magicNumber = WorstPractices.animals(false);
+		magicNumber *= 5;
+		System.out.println("Magic Number from animals method  "+magicNumber);
+		if (magicNumber > 18) {
+			while(magicNumber > 0){
+				magicNumber--;
+			}
+		}
 	}
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
-private static int animals(boolean yeswellheresthething)
-{
-		/*
-			Start off with one of these
-		*/
-	int aInt = yeswellheresthething ? 34 : 21;
+/*
+this method takes in a single parameter, animalExists. 
+Using a very elaborate and complex algorithm, it calculate a magic number
+*/
+
+private static int animals(boolean animalExists){
+
+	//If animalExists is true, then the value returned if 34 else 21.
+	int aInt = animalExists ? 34 : 21;
 	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
+	for (int brown = 0; brown < aInt; brown++) { 
+		sparklesfairy *= aInt;
+		System.out.println("sparklesfairy "+sparklesfairy);
+	} 
+	return (int) sparklesfairy * aInt;
 }
 
 	/************************************************
