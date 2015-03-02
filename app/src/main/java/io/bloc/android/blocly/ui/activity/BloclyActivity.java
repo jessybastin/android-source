0564e400208a8d0232472d77a9dd1354d2a917d1
jessybastin <jayapriya@rishis-air.attlocal.net>
2015-03-02 15:19:23 -0800

    checkpoint 36 completed

--- app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -2,27 +2,39 @@ package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
+import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 
 /**
  * Created by jayapriya on 01/03/15.
  */
 public class BloclyActivity extends Activity{
 
-    TextView textOut;
+    //TextView textOut;
+    private ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
-       textOut = (TextView)findViewById(R.id.mod_text);
-        textOut.setText(BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle());
-                Toast.makeText(this,
-                        BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
-                        Toast.LENGTH_LONG).show();
+       //textOut = (TextView)findViewById(R.id.mod_text);
+       // textOut.setText(BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle());
+              //  Toast.makeText(this,
+                //        BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
+                  //      Toast.LENGTH_LONG).show();
+
+        itemAdapter = new ItemAdapter();
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_activity_blocly);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(itemAdapter);
+
+
     }
 }
